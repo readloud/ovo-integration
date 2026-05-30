b64f30aabfdc994ae78f8e1636b596f734b56380
読み上げ <67372333+readloud@users.noreply.github.com>
2026-05-30 19:32:43 +0700

    Update Meta.php

--- src/Meta/Meta.php
+++ src/Meta/Meta.php
@@ -5,11 +5,10 @@ namespace Svn\Meta;
 
 class Meta
 {
-    const APP_VERSION = '3.45.0';
-    const APP_ID = 'ovo_android';
-    const APP_ID = 'C7UMRSMFRZ46D9GW9IK7';
     const OS_NAME = 'Android';
     const OS_VERSION = '11';
     const DEVICE_ID = 'android-ovo-default';
     const MAC_ADDRESS = '02:00:00:44:55:66';
+    const APP_VERSION = '3.45.0';
+    const APP_ID = 'ovo_android';
 }
